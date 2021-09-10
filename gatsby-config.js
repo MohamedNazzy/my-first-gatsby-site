485591e8744113eb87fab463456aae629e3e5d5c
mohamed.mhd211@gmail.com <mohamed.mhd211@gmail.com>
2021-09-10 10:11:29 -0700

    Finised data Layer

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,19 @@
 module.exports = {
   siteMetadata: {
     siteUrl: "https://www.yourdomain.tld",
-    title: "My First Gatsby Site",
+    title: "My First Gatsby Site New One",
   },
   plugins: [
     "gatsby-plugin-gatsby-cloud",
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+              path: `${__dirname}/blog`,
+              
+      }
+    },
   ],
 };
