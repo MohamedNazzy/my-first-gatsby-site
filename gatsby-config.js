54a4e63edf1d6ab3f4341d619b7d3196ccabb00f
mohamed.mhd211@gmail.com <mohamed.mhd211@gmail.com>
2021-09-10 02:31:28 -0700

    Aboutus page Updated again

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@ module.exports = {
     title: "My First Gatsby Site",
   },
   plugins: [
-    // "gatsby-plugin-gatsby-cloud",
+    "gatsby-plugin-gatsby-cloud",
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
   ],
